9bf8521016126f3196023ee659392f53a0691645
paulaResende <bellsuo@gmail.com>
2020-08-16 19:45:19 -0300

    Corrigir Profiles refatorando para Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
@@ -8,7 +12,6 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index'
   end
-  devise_for :profiles
   devise_for :admins
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
