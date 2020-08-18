a276c6e8a750eab9f15df2f9271af42d284817d3
paulaResende <bellsuo@gmail.com>
2020-08-18 13:05:18 -0300

    Refatorar controller Admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,23 +1,36 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password_blank, only: [:update]
+  before_action :set_admin, only: [:edit, update]
+
   def index
     @admins = Admin.all
   end
 
-  def edit
-    @admin = Admin.find(params[:id])
-  end
+  def edit; end
 
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
     end
   end
+
+  private
+  def verify_password_blank
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+
+  private
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  private
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
 end
