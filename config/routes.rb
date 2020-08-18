00235e49025121493d63d2095a451a9a3d8050a1
paulaResende <bellsuo@gmail.com>
2020-08-18 10:34:10 -0300

    Ajustar rota editar Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  namespace :admins_backoffice do
-    get 'admins/index'
-  end
   namespace :site do
     get 'welcome/index'
   end
+
   namespace :users_backoffice do
     get 'welcome/index'
   end
+
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' #Dashboard
+    resources :admins, only: [:index, :edit]
   end
   
   devise_for :admins
