45bd2c2a222869b1dc1ee1b49c83bb8c4a8d4193
paulaResende <bellsuo@gmail.com>
2020-08-30 13:23:44 -0300

    Criar helper para atribuir status de busca no jumbotron de acordo com método

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,13 @@
 module SiteHelper
+
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Últimas perguntas cadastradas..."
+    when 'questions'
+      "Resultados para o termo \"#{params[:term]}\"... "
+    when 'subject'
+      "Filtradas por assunto \"#{params[:subject_id]}\"..."
+    end
+  end
 end
